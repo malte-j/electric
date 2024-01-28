1452aa2e29d6413f0563fdad6d480e36795c40e2
Oleksii Sholik <oleksii@sholik.dev>
2024-01-28 23:29:31 +0200

    wip

--- examples/web-wa-sqlite/src/Example.tsx
+++ examples/web-wa-sqlite/src/Example.tsx
@@ -8,12 +8,22 @@ import { ElectricDatabase, electrify } from 'electric-sql/wa-sqlite'
 import { authToken } from './auth'
 import { Electric, Items as Item, schema } from './generated/client'
 
+import { Tetrominoes as Tetromino, } from './generated/client'
+import { tetromino as tetrominoEnum, colour as colourEnum } from './generated/client/prismaClient.d'
+
 import './Example.css'
+
+const angles = [0, 90, 180, 270]
+
+const sample = (array: any[]) => array[Math.floor(Math.random() * array.length)]
+const sampleShape = () => sample(Object.keys(tetrominoEnum))
+const sampleColour = () => sample(Object.keys(colourEnum))
+const sampleAngle = () => sample(angles)
 
 const { ElectricProvider, useElectric } = makeElectricContext<Electric>()
 
 export const Example = () => {
-  const [ electric, setElectric ] = useState<Electric>()
+  const [electric, setElectric] = useState<Electric>()
 
   useEffect(() => {
     let isMounted = true
@@ -30,7 +40,7 @@ export const Example = () => {
       const { tabId } = uniqueTabId()
       const scopedDbName = `basic-${LIB_VERSION}-${tabId}.db`
 
-      const conn = await ElectricDatabase.init(scopedDbName) 
+      const conn = await ElectricDatabase.init(scopedDbName)
       const electric = await electrify(conn, schema, config)
 
       if (!isMounted) {
@@ -60,51 +70,52 @@ export const Example = () => {
 
 const ExampleComponent = () => {
   const { db } = useElectric()!
-  const { results } = useLiveQuery(
-    db.items.liveMany()
-  )
 
   useEffect(() => {
-    const syncItems = async () => {
-      // Resolves when the shape subscription has been established.
-      const shape = await db.items.sync()
-
-      // Resolves when the data has been synced into the local database.
+    const syncTetrominoes = async () => {
+      const shape = await db.tetrominoes.sync()
       await shape.synced
     }
 
-    syncItems()
+    syncTetrominoes()
   }, [])
 
-  const addItem = async () => {
-    await db.items.create({
+  const addPiece = async () => {
+    await db.tetrominoes.create({
       data: {
-        value: genUUID(),
+        id: genUUID(),
+        shape: sampleShape(),
+        colour: sampleColour(),
+        angle: sampleAngle()
       }
     })
   }
 
-  const clearItems = async () => {
-    await db.items.deleteMany()
+  const clearPieces = async () => {
+    await db.tetrominoes.deleteMany()
   }
 
-  const items: Item[] = results ?? []
+  const liveQuery = useLiveQuery(db.tetrominoes.liveMany())
+  const pieces: Tetromino[] = liveQuery.results ?? []
 
   return (
     <div>
       <div className="controls">
-        <button className="button" onClick={ addItem }>
+        <button className="button" onClick={addPiece}>
           Add
         </button>
-        <button className="button" onClick={ clearItems }>
+        <button className="button" onClick={clearPieces}>
           Clear
         </button>
       </div>
-      {items.map((item: Item, index: number) => (
-        <p key={ index } className="item">
-          <code>{ item.value }</code>
+      {pieces.map((piece: Tetromino, index: number) =>
+        <p key={index} className="tetromino">
+          <code>{piece.id}</code>
+          <code>{piece.shape}</code>
+          <code>{piece.colour}</code>
+          <code>{piece.angle}</code>
         </p>
-      ))}
+      )}
     </div>
   )
 }
